f8305b2e650c84aaa56dd725378bf1fa209688ce
Bazzan <sebake01@gmail.com>
2020-11-25 16:26:32 +0100

    Initial commit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode { NoiseMap, ColorMap };
+    public DrawMode drawMode;
+
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+    //number of layers of noise (frequency = lacunarity ^ 0 = 1) (amplitude = persistance ^0 = 1)
+    public int octaves;
+    //contols decrease in amplitude of octaves (affects how much these small features influence the overall shape of the map)
+    [Range(0f, 1f)]
+    public float persistance;
+    //contols increase in frequency of octaves(increases the number of small features on the map)
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public TerrainType[] regions;
+
+
+    public bool autoUpdate;
+
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+
+        //checks which region the coridnate falls into
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        MapDisplay mapDisplay = GetComponent<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap)
+            mapDisplay.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        else if (drawMode == DrawMode.ColorMap)
+            mapDisplay.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+
+    }
+
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+            mapWidth = 1;
+
+        if (mapHeight < 1)
+            mapHeight = 1;
+
+        if (lacunarity < 1)
+            lacunarity = 1;
+        if (octaves < 0)
+            octaves = 0;
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
